a61def83f61d765d1704aa4efff5b80008a0d525
AdamGonda <adamgondagyula@gmail.com>
2020-10-25 13:04:14 +0100

    refactor

--- index.js
+++ index.js
@@ -1,69 +1,14 @@
-const { pipe, isInRange, awsd, QUIT } = require("./utils")
+const { pipe, isInRange, awsd, QUIT } = require('../utils')
+const EventEmitter = require('events')
 
-const BACKGROUND = " "
-const TOP_AND_BOTTOM = "-"
-const LEFT_AND_RIGHT = "|"
+const BACKGROUND = ' '
+const TOP_AND_BOTTOM = '-'
+const LEFT_AND_RIGHT = '|'
 
-function getVerticalBorder(w) {
-  return Array(w).fill(TOP_AND_BOTTOM).join("")
-}
+const exitEmitter = new EventEmitter()
+exitEmitter.on('exit', () => process.exit())
 
-function isOnScreen(obj, ds) {
-  return isInRange(-1, obj.x, ds.WIDTH) && isInRange(0, obj.y, ds.HEIGHT)
-}
-
-function showObjects(state, renderMap, ds, isReverted) {
-  return (screen) => {
-    state.forEach((obj) => {
-      if (isOnScreen(obj, ds)) {
-        screen[obj.y][obj.x + 1] = renderMap[obj.tag]
-      }
-    })
-
-    // console renders from top to bottom
-    if (isReverted){
-      return screen
-    }
-    
-    return screen.reverse()
-  }
-}
-
-function showLeftAndRightBorder(screen) {
-  return screen.map((line) =>
-    line.map((c, i, arr) =>
-      i == 0 || i == arr.length - 1 ? LEFT_AND_RIGHT : c
-    )
-  )
-}
-
-function joinChars(screen) {
-  return screen
-    .map((line, i, arr) =>
-      i == arr.length - 1 ? line.join("") : line.join("") + "\n"
-    )
-    .join("")
-}
-
-function renderToConsole(state, renderMap, ds, header, footer, isDebug, input, isReverted) {
-  let screen = pipe(
-    [...Array(ds.HEIGHT + 1)].map(() => Array(ds.WIDTH + 3).fill(BACKGROUND))
-  )(showObjects(state, renderMap, ds, isReverted), showLeftAndRightBorder, joinChars)
-
-  !isDebug && console.log("\033[2J")
-  isDebug && console.log("input", input)
-  header && header(state)
-  console.log(getVerticalBorder(ds.WIDTH + 3))
-  console.log(screen)
-  console.log(getVerticalBorder(ds.WIDTH + 3))
-  footer && footer(state)
-}
-
-const bridge = {
-  input: null,
-}
-
-exports.run = ({
+function run({
   initialState,
   update,
   toCommon,
@@ -74,33 +19,186 @@ exports.run = ({
   footer,
   updateTime = 100, // bigger is slower
   isDebug = true,
-  isReverted = false,
-}) => {
-  // RUN IN TERMINAL
+}) {
   let currentState = initialState
+  const bridge = {
+    input: null,
+  }
 
   process.stdin.setRawMode(true)
-  process.stdin.on("data", (buffer) => {
+  process.stdin.on('data', buffer => {
     let key = buffer.toJSON().data[0].toString()
-    const isGameOver = currentState.isGameOver
 
-    if (key == QUIT || isGameOver) process.exit()
-    if (Object.keys(inputMap).includes(key)) bridge.input = inputMap[key]
+    if (key == QUIT) {
+      process.exit()
+    }
+    if (Object.keys(inputMap).includes(key)) {
+      bridge.input = inputMap[key]
+    }
   })
 
   setInterval(() => {
     currentState = update(currentState, bridge.input)
-    isDebug && console.log(currentState)
+
+    if (currentState.isGameOver) {
+      exitEmitter.emit('exit')
+    }
+
     renderToConsole(
-      toCommon(currentState),
+      currentState,
+      toCommon,
       renderMap,
       dimensions,
       header,
       footer,
       isDebug,
       bridge.input,
-      isReverted,
     )
+
     bridge.input = null
   }, updateTime)
 }
+
+function renderToConsole(
+  state,
+  toCommon,
+  renderMap,
+  dimensions,
+  header,
+  footer,
+  isDebug,
+  input,
+) {
+  const { width, height } = dimensions
+  const common = toCommon(state)
+
+  const base = [...Array(height + 1)].map(() =>
+    Array(width + 3).fill(BACKGROUND),
+  )
+
+  // prettier-ignore
+  let screen = pipe(base)
+    (
+      drawToScreen(common, renderMap, dimensions),
+      showLeftAndRightBorder,
+      joinChars
+    )
+
+  if (isDebug) {
+    console.log(state)
+    console.log('input', input)
+  } else {
+    console.log('\033[2J')
+  }
+
+  header && header(state)
+  console.log(getVerticalBorder(width + 3))
+  console.log(screen)
+  console.log(getVerticalBorder(width + 3))
+  footer && footer(state)
+}
+
+function drawToScreen(state, renderMap, dimensions) {
+  return screen => {
+    state.forEach(obj => {
+      if (isOnScreen(obj, dimensions)) {
+        screen[obj.y][obj.x + 1] = renderMap[obj.tag]
+      }
+    })
+
+    return screen.reverse()
+  }
+}
+
+function isOnScreen(obj, dimensions) {
+  const { width, height } = dimensions
+  return isInRange(0, obj.x, width) && isInRange(0, obj.y, height)
+}
+
+function showLeftAndRightBorder(screen) {
+  return screen.map(line =>
+    line.map((c, idx, arr) =>
+      idx == 0 || idx == arr.length - 1 ? LEFT_AND_RIGHT : c,
+    ),
+  )
+}
+
+function joinChars(screen) {
+  return screen
+    .map((line, idx, arr) =>
+      idx == arr.length - 1 ? line.join('') : line.join('') + '\n',
+    )
+    .join('')
+}
+
+function getVerticalBorder(width) {
+  return Array(width).fill(TOP_AND_BOTTOM).join('')
+}
+
+function validateArgs(args) {
+  const {
+    initialState,
+    update,
+    toCommon,
+    renderMap,
+    inputMap,
+    dimensions,
+    header,
+    footer,
+    updateTime,
+    isDebug,
+  } = args
+
+  const msg = property =>
+    `You missed the '${property}' property from run's config, or it is not `
+
+  const isObject = value =>
+    !!value && typeof value == 'object' && !Array.isArray(value)
+
+  const isFunction = value => !!value && typeof value === 'function'
+
+  if (!isObject(initialState)) {
+    throw Error(msg('initialState') + 'an object.')
+  }
+
+  if (!isFunction(update)) {
+    throw Error(msg('update') + 'a function.')
+  }
+
+  if (!isFunction(toCommon)) {
+    throw Error(msg('toCommon') + 'a function.')
+  }
+
+  if (!isObject(renderMap)) {
+    throw Error(msg('renderMap') + 'an object.')
+  }
+  
+  if (!isObject(dimensions)) {
+    throw Error(msg('dimensions') + 'an object.')
+  }
+  
+  // optional
+  if (inputMap && !isObject(inputMap)) {
+    throw Error('inputMap is not an object.')
+  }
+
+  if (header && !isFunction(header)) {
+    throw Error('header is not a function.')
+  }
+
+  if (footer && !isFunction(footer)) {
+    throw Error('footer is not a function.')
+  }
+
+  if (updateTime && isNaN(updateTime)) {
+    throw Error('updateTime is not a number.')
+  }
+
+  if (isDebug && typeof isDebug === "boolean") {
+    throw Error('isDebug is not a boolean.')
+  }
+
+  run(args)
+}
+
+exports.run = validateArgs
--- utils.js
+++ utils.js
@@ -2,65 +2,12 @@ function pipe(seed) {
   return (...ops) => ops.reduce((state, action) => action(state), seed)
 }
 
-function find(state, tag) {
-  return state.find((x) => x.tag == tag)
-}
-
 function getRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 function isInRange(start, num, end) {
   return num >= start && num <= end
-}
-
-function isOverlap(obj1, obj2) {
-  return obj1.x == obj2.x && obj1.y == obj2.y
-}
-
-function unfoldArr(acc, x) {
-  return Array.isArray(x) ? acc.concat(x) : acc.concat([x])
-}
-
-function filterObjFields(fields, keep) {
-  return (state) => {
-    return Object.keys(state)
-      .filter((key) => (keep ? fields.includes(key) : !fields.includes(key)))
-      .reduce((clean, key) => {
-        clean[key] = state[key]
-        return clean
-      }, {})
-  }
-}
-
-function randomBool(chance) {
-  return Math.random() <= chance
-}
-
-function getRandomItemFrom(iterable) {
-  let arr = iterable
-  if (!Array.isArray(iterable)) {
-    arr = Object.values(iterable)
-  }
-  return arr[getRange(0, arr.length - 1)]
-}
-
-function hasMatch(arr1, arr2, fn) {
-  for (let i = 0; i < arr1.length; i++) {
-    for (let j = 0; j < arr2.length; j++) {
-      const a = arr1[i]
-      const b = arr2[j]
-      if (fn(a, b)) {
-        return true
-      }
-    }
-    
-  }
-  return false
-}
-
-function point(x, y) {
-  return { x, y }
 }
 
 // Accepted inputs
@@ -87,16 +34,6 @@ const awsd = {
 }
 
 exports.pipe = pipe
-exports.find = find
-exports.getRange = getRange
 exports.isInRange = isInRange
-exports.isOverlap = isOverlap
-exports.unfoldArr = unfoldArr
-exports.filterObjFields = filterObjFields
-exports.randomBool = randomBool
-exports.getRandomItemFrom = getRandomItemFrom
-exports.hasMatch = hasMatch
-exports.point = point
-exports.DIRS = DIRS
 exports.awsd = awsd
 exports.QUIT = QUIT
